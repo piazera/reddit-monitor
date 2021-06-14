4de16d3f8cd5b18af694b3b6606b0d333b7732cc
Marcos Silva Piazera <marcos.piazera@gmail.com>
2021-06-14 20:36:10 -0300

    add 'after' reddit property

--- lib/reddit_bridge.rb
+++ lib/reddit_bridge.rb
@@ -11,27 +11,30 @@ class RedditBridge
         response = HTTParty.get('http://www.reddit.com/search.json', :query => query, :headers => headers)
         results = JSON.parse(response.body)
         return [] if results == []
-        output = results['data']['children'].map do |item|
+        children = results['data']['children'].map do |item|
             data = item['data']
             {
-                "subreddit" => data['subreddit'],
-                "author_fullname" => data['author_fullname'],
-                "title" => data['title'],
-                "subreddit_name_prefixed" => data['subreddit_name_prefixed'],
-                "name" => data['name'],
-                "subreddit_type" => data['subreddit_type'],
-                "subreddit_id" => data['subreddit_id'],
-                "id" => data['id'],
-                "author" => data['author'],
-                "num_comments" => data['num_comments'],
-                "url" => data['url'],
-                "subreddit_subscribers" => data['subreddit_subscribers'],
-                "created_utc" => data['created_utc'],
-                "num_crossposts" => data['num_crossposts'],
-                "media" => data['media'],
-                "is_video" => data['is_video']
+                :subreddit => data['subreddit'],
+                :author_fullname => data['author_fullname'],
+                :title => data['title'],
+                :subreddit_name_prefixed => data['subreddit_name_prefixed'],
+                :name => data['name'],
+                :subreddit_type => data['subreddit_type'],
+                :subreddit_id => data['subreddit_id'],
+                :id => data['id'],
+                :author => data['author'],
+                :num_comments => data['num_comments'],
+                :url => data['url'],
+                :subreddit_subscribers => data['subreddit_subscribers'],
+                :created_utc => data['created_utc'],
+                :num_crossposts => data['num_crossposts'],
+                :media => data['media'],
+                :is_video => data['is_video']
             }
         end
-        output
+        {
+            :after => results['data']['after'],
+            :children => children
+        }
     end
 end
--- spec/reddit_bridge_spec.rb
+++ spec/reddit_bridge_spec.rb
@@ -3,10 +3,11 @@ require "reddit_bridge"
 describe RedditBridge do
     describe "reddit bridge" do
       let(:response) { RedditBridge.new.search("cpi") }
-      it "returns correctly some data" do
-        expect(response.length()).to eq(5)
-        expect(response[2]["author"]).to eq("IamnottheJoe")
-        expect(response[4]["url"]).to eq("https://www.reddit.com/r/EnquetesBrasil/comments/nps78o/cpi_da_pandemia_nise_yamaguchi/")
+      it "returns data correctly" do
+        expect(response[:after]).to eq('t3_nps78o')
+        expect(response[:children].length()).to eq(5)
+        expect(response[:children][2][:author]).to eq("IamnottheJoe")
+        expect(response[:children][4][:url]).to eq("https://www.reddit.com/r/EnquetesBrasil/comments/nps78o/cpi_da_pandemia_nise_yamaguchi/")
       end
     end
 end
